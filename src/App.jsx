4f13913b5acb8b59a5479efbc40797bafa77725e
mdasadofficial <mdasadofficial1@gmail.com>
2026-03-30 05:34:13 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Navbar from './NavBar/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar/>
+      <h1 className='text-red-500 bg-black text-center py-8 font-bold text-5xl'>
+        Hello... Hi
+
+      </h1>
+    </>
+  )
+}
+
+export default App
